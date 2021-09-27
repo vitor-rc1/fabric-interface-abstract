04065898cda4d2729bb6040b5c9305ac8205b2ca
Vitor R <vitor_rc@outlook.com>
2021-09-27 17:39:14 -0300

    Interface generica

--- ICarros.cs
+++ ICarros.cs
@@ -10,7 +10,7 @@ namespace Fabrica
     {
         int Id { get; set; }
         string Nome { get; set; }
-        string Nome { get; set; }
-        string Nome { get; set; }
+        string Modelo { get; set; }
+        int Ano { get; set; }
     }
 }
