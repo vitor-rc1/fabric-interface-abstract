071fa64c35b57bea49c56903a69124e9b4eab55a
Vitor R <vitor_rc@outlook.com>
2021-09-27 17:31:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fabrica
+{
+    class Program
+    {
+        public static readonly string SqlCNN = "Server=localhost;database=aula_xp;user=root;password=root;SslMode=none";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
